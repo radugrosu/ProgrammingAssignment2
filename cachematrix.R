0f37655a1bee7d12e355ba3ad4b70f5963223d40
radugrosu <radu.grosu@gmail.com>
2014-06-15 14:36:41 +0200

    added function comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 makeCacheMatrix <- function(mat = matrix()) {
-    ## Takes the matrix provided as input and provides an object (R list)
-    ## that stores the (computationally expensive) result of matrix inversion
-    ## and makes it available as long as the initial matrix doesn't change.
-    ## The values of the matrix and its inverse are read/written through
-    ## getters and setters which act on the `mat' and `inv' values in the
-    ## local environment.
+    ## Takes in a matrix and returns a cache object (using an R list)
+    ## designed to facilitate the storage and retrieval of a
+    ## computationally expensive result (e.g. matrix inversion).
+    ## The local environment holds two local variables: the input value `mat' 
+    ## and the value of interest `inv' and provides read/write access to them
+    ## through corresponding getters and setters.
     inv = NULL
     get = function() mat
     set = function(nmat) {mat <<- nmat; inv <<- NULL}
@@ -16,9 +16,9 @@ makeCacheMatrix <- function(mat = matrix()) {
 cacheSolve <- function(cmat, ...) {
     ## Takes in an object of type CacheMatrix (typically an R list created
     ## by the companion function makeCacheMatrix) and tries to retrieve the
-    ## stored value of the inverse; if this value is not available it computes
-    ## the inverse, stores the result in the corresponding slot of the input 
-    ## object and returns the result.
+    ## stored value of interest (the matrix inverse); if this value is not
+    # available it reads in the matrix, computes the inverse, stores the result 
+    # in the corresponding slot of the cache object and returns the result.
     inv = cmat$getinv()
     if(!is.null(inv)) return(inv)
     ninv = solve(cmat$get(), ...)
